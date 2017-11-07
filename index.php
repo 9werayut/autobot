2e9d17dbe8288c41c465c8a2bd1bc0a9e1543f66
werayut.ku <werayut.ku@gmail.com>
2017-11-07 14:48:38 +0700

    update miniOperator again

--- index.php
+++ index.php
@@ -13,31 +13,35 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);  
 if (!is_null($events['events'])) 
 {      
-    //Line API send a lot of event type, we interested in message only.         
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {              
-        // Get replyToken             
-        $replyToken = $event['replyToken'];              
-        switch($event['message']['text']) {                  
-            case 'tel':                     
-                $respMessage = '089-5124512';                     
-            break;                 
-            case 'address':                     
-                $respMessage = '99/451 Muang Nonthaburi';                     
-            break;                 
-            case 'boss':                     
-                $respMessage = '089-2541545';                     
-            break;                 
-            case 'idcard':                     
-                $respMessage = '5845122451245';                     
-            break;                 
-            default:
-                $respMessage = 'ไม่มีข้อมูลที่ต้องการครับ';                  
-            break;             
-        }              
-        $httpClient = new CurlHTTPClient($channel_token);             
-        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));              
-        $textMessageBuilder = new TextMessageBuilder($respMessage);             
-        $response = $bot->replyMessage($replyToken, $textMessageBuilder);          } 
+    //Loop through each event     
+    foreach ($events['events'] as $event) {
+        //Line API send a lot of event type, we interested in message only.         
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {              
+            // Get replyToken             
+            $replyToken = $event['replyToken'];              
+            switch($event['message']['text']) {                  
+                case 'tel':                     
+                    $respMessage = '089-5124512';                     
+                break;                 
+                case 'address':                     
+                    $respMessage = '99/451 Muang Nonthaburi';                     
+                break;                 
+                case 'boss':                     
+                    $respMessage = '089-2541545';                     
+                break;                 
+                case 'idcard':                     
+                    $respMessage = '5845122451245';                     
+                break;                 
+                default:
+                    $respMessage = 'ไม่มีข้อมูลที่ต้องการครับ';                  
+                break;             
+            }              
+            $httpClient = new CurlHTTPClient($channel_token);             
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));              
+            $textMessageBuilder = new TextMessageBuilder($respMessage);             
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);          
+        } 
+    }
 } 
 
 echo "OK"; 
