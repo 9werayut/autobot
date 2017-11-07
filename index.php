47893b8de4725f26eb3584952e648de59315d596
werayut.ku <werayut.ku@gmail.com>
2017-11-07 14:33:37 +0700

    update for Project: mini Operator

--- index.php
+++ index.php
@@ -13,27 +13,30 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);  
 if (!is_null($events['events'])) 
 {      
-    // Loop through each event     
-    foreach ($events['events'] as $event) {          
-        // Line API send a lot of event type, we interested in message only.         
-        if ($event['type'] == 'message') {              
-            // Get replyToken             
-            $replyToken = $event['replyToken'];              
-            switch($event['message']['type']) {                  
-                case 'image':                     
-                    $messageID = $event['message']['id'];                     
-                    $respMessage = 'Hello, your image ID is '. $messageID;
-                break;                 
-                default:                     
-                    $respMessage = 'Please send image only';
-                break;             
-            }              
-            $httpClient = new CurlHTTPClient($channel_token);             
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
-            $textMessageBuilder = new TextMessageBuilder($respMessage);             
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);         
-        }     
-    } 
+    //Line API send a lot of event type, we interested in message only.         
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {              
+        // Get replyToken             
+        $replyToken = $event['replyToken'];              
+        switch($event['message']['text']) {                  
+            case 'tel':                     
+                $respMessage = '089-5124512';                     
+            break;                 
+            case 'address':                     
+                $respMessage = '99/451 Muang Nonthaburi';                     
+            break;                 
+            case 'boss':                     
+                $respMessage = '089-2541545';                     
+            break;                 
+            case 'idcard':                     
+                $respMessage = '5845122451245';                     
+            break;                 
+            default:                     
+            break;             
+        }              
+        $httpClient = new CurlHTTPClient($channel_token);             
+        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));              
+        $textMessageBuilder = new TextMessageBuilder($respMessage);             
+        $response = $bot->replyMessage($replyToken, $textMessageBuilder);          } 
 } 
 
 echo "OK"; 
